87f9dd2cc85bfd2b7363595bfc8d3bd7f367335f
Rikuya Suzuki <s11rikuya@gmail.com>
2019-10-19 17:36:44 +0900

    add google map api

---
+++ src/components/home/GMap.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="map">
+    <h2>Map</h2>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name:'GMap',
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
