e0670a76c4a99e5b782a9b913aeed34949ef8c1f
Rikuya Suzuki <s11rikuya@gmail.com>
2019-10-19 17:10:11 +0900

    add navbar component

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+<div class="navbar">
+  <nav class="deep-purple darken-1">
+    <div class="container">
+      <a href="#" clss="brand-logo left">GeoNinjas!</a>
+      <ul clss="right">
+        <li><a href="#">Signup</a></li>
+        <li><a href="#">Login</a></li>
+      </ul>
+    </div>
+  </nav>
+
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
